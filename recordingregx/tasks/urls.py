63e4bbc9278a83ab5ecdd01059b5d6eda86a8ffd
Yanujz <yanujz@live.it>
2022-12-08 00:18:35 +0100

    first test crud working

--- recordingregx/tasks/urls.py
+++ recordingregx/tasks/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
-from .views import TasksView
+# from .views import TasksView
+from . import views
 
-urlpatterns =[
-    path('', TasksView.as_view(http_method_names=['get'])),
-    path('/<int:id>/', TasksView.as_view(http_method_names=['get'])),
-    path('/store', TasksView.as_view(http_method_names=['post'])),
-    path('/update/<int:id>', TasksView.as_view(http_method_names=['put'])),
-    path('/delete/<int:id>', TasksView.as_view(http_method_names=['delete'])),
+# urlpatterns =[
+#     path('', TasksView.as_view(http_method_names=['get'])),
+#     path('/<int:id>/', TasksView.as_view(http_method_names=['get'])),
+#     path('/store', TasksView.as_view(http_method_names=['post'])),
+#     path('/update/<int:id>', TasksView.as_view(http_method_names=['put'])),
+#     path('/delete/<int:id>', TasksView.as_view(http_method_names=['delete'])),
+# ]
+
+# namespace
+app_name = 'tasks'
+
+urlpatterns = [
+    path('', views.TaskListView.as_view(), name='task_list'),
+    path('/store', views.TaskCreateView.as_view(), name='task_create'),
+    path('/update/<int:pk>', views.TaskUpdateView.as_view(), name='task_update'),
+    path('/delete/<int:pk>', views.TaskDeleteView.as_view(), name='task_delete'),
 ]
--- recordingregx/tasks/views.py
+++ recordingregx/tasks/views.py
@@ -1,23 +1,77 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 import json
+from .forms import TaskForm
+from .models import Task
+from django.views.generic import ListView, DetailView, \
+    CreateView, UpdateView, DeleteView
 
 # @csrf_exempt
-class TasksView(View):
-    def get(self, request, id=None):
-        return HttpResponse('GET request')
+# class TasksView(View):
+#     def get(self, request, id=None):
+#         # return HttpResponse('GET request')
+#         # return render(request, "index.html") # add this line
+#         form = TaskForm()
+#         tasks = Task.objects.all() # add this
+#         return render(request, "index.html", {"task_form": form, "tasks": tasks, "action_uri": "/tasks/store"})
+
     
-    def post(self, request):
-        body_unicode = request.body.decode('utf-8')
-        body = json.loads(body_unicode)
-        # content = body['content']
-        return HttpResponse('POST request\r\n {}'.format(body))
+#     def post(self, request):
+#         form = TaskForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("/tasks")
+        
+#         # body_unicode = request.body.decode('utf-8')
+#         # body = json.loads(body_unicode)
+#         # content = body['content']
+#         # return HttpResponse('POST request\r\n {}'.format(body))
 
-    def put(self, request, id):
-        return HttpResponse('PUT request id: {}'.format(id))
+#     def put(self, request, id):
+#         return HttpResponse('PUT request id: {}'.format(id))
 
-    def delete(self, request, id):
-        return HttpResponse('DELETE request')
+#     def delete(self, request, id):
+#         return HttpResponse('DELETE request')
+
+class TaskListView(ListView):
+    paginate_by = 2
+    model = Task
+    form = TaskForm
+    context_object_name = 'tasks'
+    template_name = "task_list.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(TaskListView, self).get_context_data(**kwargs)
+        context['action_uri'] = "/tasks/store"
+        context['task_form'] = TaskForm()
+        return context
+    
+    def get_paginate_by(self, queryset):
+        """
+        Paginate by specified value in querystring, or use default class property value.
+        """
+        return self.request.GET.get('paginate_by', self.paginate_by)
+
+class TaskDetailView(DetailView):
+    model = Task
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "task_form.html"
+    success_url = reverse_lazy('tasks:task_list')
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "task_form.html"
+    success_url = reverse_lazy('tasks:task_list')
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = "task_confirm_delete.html"
+    success_url = reverse_lazy('tasks:task_list')
